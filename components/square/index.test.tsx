4af8a6646ea2a2d7603edc25e3e6f313f0f52144
Iuliu Visovan <iuliu.visovan@usource.ro>
2020-12-26 22:13:28 +0200

    add snapshot tests

--- components/square/index.test.tsx
+++ components/square/index.test.tsx
@@ -1,3 +1,11 @@
-test('it works', () => {
-  expect(true).toBeTruthy();
-})
+import React from 'react';
+import { create } from 'react-test-renderer';
+import Square from './index';
+
+const boardTheme = { darkSquareColor: '#b98661', lightSquareColor: '#f4d9b7' };
+
+const tree = create(<Square index={3} theme={boardTheme} />);
+
+test('snapshot', () => {
+  expect(tree).toMatchSnapshot();
+});
