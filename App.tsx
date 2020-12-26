b4ac3ae8c03f70264239372d282da62567e05ee8
Iuliu Visovan <iuliu.visovan@usource.ro>
2020-12-27 00:27:16 +0200

    fix file notations and orientation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
-import Board, { BoardRotation } from './components/board';
+import Board, {  BoardOrientation } from './components/board';
 import { THEMES } from './constants';
 
 export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Board rotation={BoardRotation.ForWhite} theme={THEMES.chessableMasters} />
+      <Board rotation={BoardOrientation.ForWhite} theme={THEMES.chessableMasters} />
       <StatusBar style="auto" />
     </View>
   );
--- components/board/index.tsx
+++ components/board/index.tsx
@@ -5,13 +5,23 @@ import Square from '../square';
 import styles from './styles';
 
 export default function Board(props: IBoardProps) {
-  function getRankIndicators() {
+  function getRankNotations() {
     const ranks = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
-    if (props.rotation === BoardRotation.ForWhite) {
+    if (props.rotation === BoardOrientation.ForWhite) {
       return ranks.reverse();
     } else {
       return ranks;
+    }
+  }
+
+  function getFileNotations() {
+    const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+    if (props.rotation === BoardOrientation.ForWhite) {
+      return files;
+    } else {
+      return files.reverse();
     }
   }
 
@@ -21,12 +31,12 @@ export default function Board(props: IBoardProps) {
         <Square key={i} index={i} theme={props.theme} />
       ))}
       <View style={styles.rankIndicators}>
-        {getRankIndicators().map((rank) => (
+        {getRankNotations().map((rank) => (
           <Text style={styles.indicator}>{rank}</Text>
         ))}
       </View>
       <View style={styles.fileIndicators}>
-        {['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].map((file) => (
+        {getFileNotations().map((file) => (
           <Text style={styles.indicator}>{file.toUpperCase()}</Text>
         ))}
       </View>
@@ -39,12 +49,12 @@ export interface ITheme {
   lightSquareColor: string;
 }
 
-export enum BoardRotation {
+export enum BoardOrientation {
   ForWhite,
   ForBlack,
 }
 
 interface IBoardProps {
   theme: ITheme;
-  rotation: BoardRotation;
+  rotation: BoardOrientation;
 }
